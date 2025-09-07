1f5052ad1d77ce589f83dc49ad5c3cbc3d9a5150
Atlas16A <thegoldenatlaspass@gmail.com>
2025-09-07 12:40:23 -0400

    Make editor a newtype wrapper to simplify the usage api

--- src/editor.rs
+++ src/editor.rs
@@ -24,3 +24,75 @@ fn setup(registry: Res<AppTypeRegistry>) {
         }
     });
 }
+
+use argh::FromArgs;
+use bevy::{
+    app::{Plugins, PluginsState},
+    prelude::*,
+};
+
+#[derive(FromArgs)]
+/// Main command line arguments
+struct Args {
+    #[argh(switch, short = 'e')]
+    /// whether to run as the editor or the game
+    editor_mode: bool,
+}
+
+pub struct EditorApp(App);
+
+pub trait EditorAppExt {
+    fn new() -> Self;
+
+    fn add_editor_plugins<M>(&mut self, plugins: impl Plugins<M>) -> &mut Self;
+
+    fn add_plugins<M>(&mut self, plugins: impl Plugins<M>) -> &mut Self;
+
+    fn run(&mut self) -> AppExit;
+}
+
+impl EditorAppExt for EditorApp {
+    fn new() -> Self {
+        Self(App::new())
+    }
+
+    fn add_editor_plugins<M>(&mut self, plugins: impl Plugins<M>) -> &mut Self {
+        let args = argh::from_env::<Args>();
+        if !args.editor_mode {
+            return self;
+        }
+        if matches!(
+            self.0.plugins_state(),
+            PluginsState::Cleaned | PluginsState::Finished
+        ) {
+            panic!(
+                "Plugins cannot be added after App::cleanup() or App::finish() has been called."
+            );
+        }
+
+        plugins.add_to_app(&mut self.0);
+        self
+    }
+
+    fn add_plugins<M>(&mut self, plugins: impl Plugins<M>) -> &mut Self {
+        let args = argh::from_env::<Args>();
+        if args.editor_mode {
+            return self;
+        }
+        if matches!(
+            self.0.plugins_state(),
+            PluginsState::Cleaned | PluginsState::Finished
+        ) {
+            panic!(
+                "Plugins cannot be added after App::cleanup() or App::finish() has been called."
+            );
+        }
+
+        plugins.add_to_app(&mut self.0);
+        self
+    }
+
+    fn run(&mut self) -> AppExit {
+        self.0.run()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
-use argh::FromArgs;
 use bevy::prelude::*;
 use bevy::remote::RemotePlugin;
 
 mod game;
 use game::GamePlugin;
 mod editor;
-use editor::EditorPlugin;
+use editor::{EditorApp, EditorAppExt, EditorPlugin};
+
+/*Standard bevy app main
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+*/
 
 fn main() {
-    let args = argh::from_env::<Args>();
-    let mut app = App::new();
-    if args.editor_mode {
-        println!("Running in editor mode");
-        app.add_plugins(EditorPlugin);
-    } else {
-        println!("Running in game mode");
-        app.add_plugins(GamePlugin)
-            .add_plugins(RemotePlugin::default());
-    }
-    app.run();
-}
-
-#[derive(FromArgs)]
-/// Main command line arguments
-struct Args {
-    #[argh(switch, short = 'e')]
-    /// whether to run as the editor or the game
-    editor_mode: bool,
+    EditorApp::new()
+        .add_plugins(GamePlugin)
+        .add_editor_plugins(EditorPlugin)
+        .add_plugins(RemotePlugin::default())
+        .run();
 }
